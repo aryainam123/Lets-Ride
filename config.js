6785b08ecb298a6690d14a88f77d3fc385e415cb
aryainam123 <82810428+aryainam123@users.noreply.github.com>
2021-10-12 11:13:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,18 @@ require("@firebase/firestore")
 
 const firebaseConfig = {
  //Add configuration here
+ apiKey: "AIzaSyBz2HPG0Nl3jEA4nlHnwHeRvaQsp0ZdCK0",
+
+  authDomain: "cycle-459c3.firebaseapp.com",
+
+  projectId: "cycle-459c3",
+
+  storageBucket: "cycle-459c3.appspot.com",
+
+  messagingSenderId: "43309807123",
+
+  appId: "1:43309807123:web:c59ba65b1bdfaa285071e6"
+
  
 };
   firebase.initializeApp(firebaseConfig);
